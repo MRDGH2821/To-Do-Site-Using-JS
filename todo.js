e46e1856c8cde4e8018fcc20db09fcde61682b8c
Mihir Rabade <mihirrabade@gmail.com>
2021-08-11 15:07:06 +0530

    Beautified files

--- todo.js
+++ todo.js
@@ -2,36 +2,35 @@ var addButton = document.getElementById("addButton");
 var toDoList = document.getElementById("toDoList");
 var todoInput = document.getElementById("todoInput");
 
-function markAsDone(event){
-
-    event.target.parentElement.classList.add("markAsDone")
+function markAsDone(event)
+{
+	event.target.parentElement.classList.add("markAsDone")
 }
 
-function addToDoList(){
-    var toDoMain = document.createElement('div')
-    toDoMain.id = String(Math.random());
+function addToDoList()
+{
+	var toDoMain = document.createElement('div')
+	toDoMain.id = String(Math.random());
 
-    var todo = document.createElement('div');
-    todo.appendChild(document.createTextNode(todoInput.value));
-    toDoMain.appendChild(todo);
+	var todo = document.createElement('div');
+	todo.appendChild(document.createTextNode(todoInput.value));
+	toDoMain.appendChild(todo);
 
-    var todoX = document.createElement('div');
-    todoX.appendChild(document.createTextNode('X'));
-    toDoMain.appendChild(todoX);
- 
+	var todoX = document.createElement('div');
+	todoX.appendChild(document.createTextNode('X'));
+	toDoMain.appendChild(todoX);
 
+	var todoEdit = document.createElement('div');
+	todoEdit.appendChild(document.createTextNode('Edit'));
+	toDoMain.appendChild(todoEdit);
 
-    var todoEdit = document.createElement('div');
-    todoEdit.appendChild(document.createTextNode('Edit'));
-    toDoMain.appendChild(todoEdit);
-
-    var todoDone = document.createElement('div');
-    todoDone.appendChild(document.createTextNode('Done'));
-    toDoMain.appendChild(todoDone);
-    todoDone.addEventListener("click", markAsDone );
+	var todoDone = document.createElement('div');
+	todoDone.appendChild(document.createTextNode('Done'));
+	toDoMain.appendChild(todoDone);
+	todoDone.addEventListener("click", markAsDone);
 
 
-    toDoList.appendChild(toDoMain);
+	toDoList.appendChild(toDoMain);
 }
 
-addButton.addEventListener("click", addToDoList );
+addButton.addEventListener("click", addToDoList);
