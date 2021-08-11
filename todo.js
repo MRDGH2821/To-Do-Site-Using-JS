26e5b5f57515490853c069aa2bcaf0e89d6e63ff
Mihir Rabade <mihirrabade@gmail.com>
2021-08-11 14:58:08 +0530

    Added base files

---
+++ todo.js
@@ -0,0 +1,37 @@
+var addButton = document.getElementById("addButton");
+var toDoList = document.getElementById("toDoList");
+var todoInput = document.getElementById("todoInput");
+
+function markAsDone(event){
+
+    event.target.parentElement.classList.add("markAsDone")
+}
+
+function addToDoList(){
+    var toDoMain = document.createElement('div')
+    toDoMain.id = String(Math.random());
+
+    var todo = document.createElement('div');
+    todo.appendChild(document.createTextNode(todoInput.value));
+    toDoMain.appendChild(todo);
+
+    var todoX = document.createElement('div');
+    todoX.appendChild(document.createTextNode('X'));
+    toDoMain.appendChild(todoX);
+ 
+
+
+    var todoEdit = document.createElement('div');
+    todoEdit.appendChild(document.createTextNode('Edit'));
+    toDoMain.appendChild(todoEdit);
+
+    var todoDone = document.createElement('div');
+    todoDone.appendChild(document.createTextNode('Done'));
+    toDoMain.appendChild(todoDone);
+    todoDone.addEventListener("click", markAsDone );
+
+
+    toDoList.appendChild(toDoMain);
+}
+
+addButton.addEventListener("click", addToDoList );
